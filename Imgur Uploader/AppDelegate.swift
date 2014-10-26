c2f496e93401e2697b5cf3069a06c00c3ab411ae
Nelson Pecora <nelson@yoshokatana.com>
2014-10-26 13:36:44 -0400

    Initial Commit

---
+++ Imgur Uploader/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Imgur Uploader
+//
+//  Created by Nelson Pecora on 10/26/14.
+//  Copyright (c) 2014 Nelson Pecora. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
