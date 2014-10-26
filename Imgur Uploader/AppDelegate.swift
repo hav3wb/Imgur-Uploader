40c71d96e995b50223cd35505c6143083b91515b
Nelson Pecora <nelson@yoshokatana.com>
2014-10-26 13:59:58 -0400

    proper images

--- Imgur Uploader/AppDelegate.swift
+++ Imgur Uploader/AppDelegate.swift
@@ -10,9 +10,28 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
+    
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var menuItem : NSMenuItem = NSMenuItem()
+    
+    // status bar images
+    var imgurIcon = NSImage.init?(byReferencingFile "images.xcassets": String)
+    
+    override func awakeFromNib() {
+        //Add statusBarItem
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.title = "Presses"
+        statusBarItem.image =
+        
+        //Add menuItem to menu
+        menuItem.title = "Select Image"
+        menuItem.action = Selector("setWindowVisible:")
+        menuItem.keyEquivalent = ""
+        menu.addItem(menuItem)
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
